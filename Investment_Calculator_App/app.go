c385020ad423f627feb7c34e136893aa289a3eea
rpraveenkumar1203 <rpraveenkumar1203@outlook.com>
2024-08-25 17:19:02 +0530

    getting output of expected return with value hard coded value

---
+++ Investment_Calculator_App/app.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+var print = fmt.Print
+var println = fmt.Println
+
+func main() {
+	println("Building investment Calculator APP")
+
+	// input
+
+	var investmentAmount float64 = 100000
+	var returnRate float64 = 5.5
+	var tenure float64 = 10
+
+	// output
+	var expectedReturnAmount = investmentAmount * math.Pow(1+returnRate/100, tenure)
+
+	println(expectedReturnAmount)
+}
---
+++ Investment_Calculator_App/go.mod
@@ -0,0 +1,3 @@
+module github.com/rpraveenkumar1203/GOLANG/Investment_Calculator_App/Investment_Calculator_App
+
+go 1.22.5
