6f1b08e6889f61df384e7b36a5abf4e8a3560228
rpraveenkumar1203 <rpraveenkumar1203@outlook.com>
2024-09-13 08:07:51 +0530

    GOLANG added file read and write

--- CLI_APPS/Bank/app.go
+++ CLI_APPS/Bank/app.go
@@ -1,10 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const balanceFile = "balance.txt"
+
+func getbalance() (float64, error) {
+
+	file, err := os.ReadFile(balanceFile)
+
+	if err != nil {
+		return 1110, errors.New("No file found")
+	}
+	balanceData := string(file)
+	balanceAmount, err := strconv.ParseFloat(balanceData, 64)
+
+	if err != nil {
+		return 1110, errors.New("Unable to parse the balance ")
+	}
+	return balanceAmount, nil
+
+}
+
+func updateBalance(balance float64) {
+
+	balanceAmount := fmt.Sprint(balance)
+	os.WriteFile(balanceFile, []byte(balanceAmount), 777)
+
+}
 
 func main() {
 
-	var userAmount float64 = 1000
+	//var userAmount float64 = 1000
+	var userAmount, err = getbalance()
+
+	if err != nil {
+		fmt.Println("---------------Database not found --------")
+		panic("No data found")
+	}
 
 	fmt.Println("Please select from below options :")
 
@@ -29,6 +66,8 @@ func main() {
 			fmt.Scan(&depositAmount)
 			if !(depositAmount <= 100) {
 				userAmount = userAmount + depositAmount
+				updateBalance(userAmount)
+
 				fmt.Printf("The total amount after deposit is %.2f \n", userAmount)
 
 			} else {
@@ -43,6 +82,7 @@ func main() {
 
 			if !(withdrawAmount > userAmount) && !(withdrawAmount <= 50) {
 				userAmount = userAmount - withdrawAmount
+				updateBalance(userAmount)
 				fmt.Printf("The total amount after withdrawl is %.2f \n", userAmount)
 			} else {
 				fmt.Printf("Please enter between 50 dollars and  %.2f dollars \n", userAmount)
@@ -52,14 +92,17 @@ func main() {
 		} else if userChoice == 3 {
 
 			fmt.Printf("Your current balance is %.2f \n", userAmount)
-			break
+			continue
 
-		} else {
-			fmt.Println("Logged out of session successfully")
+		} else if userChoice == 4 {
+			fmt.Println("\nLogged out of session successfully")
 			break
+		} else {
+			fmt.Println("Invalid input")
+			continue
 		}
 
 	}
-	fmt.Println("Thanks for using our service")
+	fmt.Println("\nThanks for using our service")
 
 }
