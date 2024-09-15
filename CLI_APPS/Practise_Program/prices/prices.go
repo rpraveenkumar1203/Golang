83e6fd4699a4ba2824cae1199e2a06d4ca9a1f68
rpraveenkumar1203 <rpraveenkumar1203@outlook.com>
2024-09-15 22:31:59 +0530

    getttin price form textfile

--- CLI_APPS/Practise_Program/prices/prices.go
+++ CLI_APPS/Practise_Program/prices/prices.go
@@ -1,6 +1,11 @@
 package prices
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 type taxwithprice struct {
 	prices     []float64
@@ -8,12 +13,48 @@ type taxwithprice struct {
 	totalprice map[string]float64
 }
 
+func (t *taxwithprice) LoadData() {
+
+	dataFile, err := os.Open("prices.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	datascanned := bufio.NewScanner(dataFile)
+
+	var ScannedData []string
+
+	for datascanned.Scan() {
+		ScannedData = append(ScannedData, datascanned.Text())
+	}
+	err = datascanned.Err()
+	if err != nil {
+		fmt.Println(err)
+		dataFile.Close()
+		return
+	}
+
+	prices := make([]float64, len(ScannedData))
+
+	for priceindex, price := range ScannedData {
+		floatPrice, err := strconv.ParseFloat(price, 64)
+
+		if err != nil {
+			fmt.Println(err)
+			dataFile.Close()
+		}
+		prices[priceindex] = floatPrice
+	}
+	t.prices = prices
+
+}
+
 func (t taxwithprice) PriceafterTax() {
+	t.LoadData()
 
 	priceaftertax := make(map[string]float64)
 
 	for _, price := range t.prices {
-		priceaftertax[fmt.Sprintf("%.2f", price)] = price * (1 * t.taxrate)
+		priceaftertax[fmt.Sprintf("%.2f", price)] = price * (1 + t.taxrate)
 	}
 	fmt.Println(priceaftertax)
 
