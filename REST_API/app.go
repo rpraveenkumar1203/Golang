a6e0b1cc725865807bb3fdbcf4280247b94c55d3
rpraveenkumar1203 <rpraveenkumar1203@outlook.com>
2024-09-21 14:48:02 +0530

    Refactoring the code

--- REST_API/app.go
+++ REST_API/app.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"strconv"
-	"time"
-
 	"github.com/gin-gonic/gin"
 	"github.com/rpraveenkumar1203/Golang/tree/main/REST_API/db"
-	"github.com/rpraveenkumar1203/Golang/tree/main/REST_API/models"
+	"github.com/rpraveenkumar1203/Golang/tree/main/REST_API/routes"
 )
 
 func main() {
@@ -16,63 +11,7 @@ func main() {
 
 	db.InitDB()
 	gin.SetMode(gin.ReleaseMode)
-
 	server := gin.Default()
-
-	server.GET("/events", getEvents)
-	server.GET("/events/:id", getEvent)
-	server.POST("/events", createEvent)
-
+	routes.RegisterRoutes(server)
 	server.Run(":8080")
-}
-
-func getEvents(context *gin.Context) {
-
-	events, err := models.GetAllEvents()
-
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could fetch events"})
-	}
-	context.JSON(http.StatusOK, events)
-
-}
-
-func getEvent(context *gin.Context) {
-
-	eventid, err := strconv.ParseInt(context.Param("id"), 10, 64)
-
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": fmt.Sprintf("cannot convert eventid to %v int ", eventid)})
-		return
-
-	}
-
-	event, err := models.GetEventbyID(eventid)
-
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": fmt.Sprintf("No data for %v ", eventid)})
-		return
-	}
-	context.JSON(http.StatusOK, event)
-
-}
-
-func createEvent(context *gin.Context) {
-
-	var event models.Event
-	err := context.ShouldBindJSON(&event)
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{
-			"message": "Could not parse request data.",
-			"error":   err.Error(),
-		})
-		return
-	}
-
-	event.ID = 1
-	event.UserID = 1
-	event.DateTime = time.Now()
-	event.Save()
-	context.JSON(http.StatusAccepted, gin.H{"message": "Event Created", "event": event})
-
 }
---
+++ REST_API/routes/events.go
@@ -0,0 +1,94 @@
+package routes
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rpraveenkumar1203/Golang/tree/main/REST_API/models"
+)
+
+func getEvents(context *gin.Context) {
+
+	events, err := models.GetAllEvents()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could fetch events"})
+	}
+	context.JSON(http.StatusOK, events)
+
+}
+
+func getEvent(context *gin.Context) {
+
+	eventid, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": fmt.Sprintf("cannot convert eventid to %v int ", eventid)})
+		return
+
+	}
+
+	event, err := models.GetEventbyID(eventid)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": fmt.Sprintf("No data for %v ", eventid)})
+		return
+	}
+	context.JSON(http.StatusOK, event)
+
+}
+
+func createEvent(context *gin.Context) {
+
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "Could not parse request data.",
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	event.DateTime = time.Now()
+	event.Save()
+	context.JSON(http.StatusAccepted, gin.H{"message": "Event Created", "event": event})
+
+}
+
+func updateEvent(context *gin.Context) {
+
+	eventid, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": fmt.Sprintf("cannot convert eventid to %v int ", eventid)})
+		return
+
+	}
+
+	_, err = models.GetEventbyID(eventid)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": fmt.Sprintf("No data for %v ", eventid)})
+		return
+
+	}
+
+	var updateEvent models.Event
+
+	err = context.ShouldBindBodyWithJSON(&updateEvent)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "Could not parse request data.",
+			"error":   err.Error(),
+		})
+		return
+	}
+
+}
