b206479a51f8cd00430d433e75a0713f3ecd6bf3
rpraveenkumar1203 <rpraveenkumar1203@outlook.com>
2024-09-22 15:39:50 +0530

    checked with aUTHORIZATION TOKEN

--- REST_API/routes/events.go
+++ REST_API/routes/events.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/rpraveenkumar1203/Golang/tree/main/REST_API/models"
+	"github.com/rpraveenkumar1203/Golang/tree/main/REST_API/utils"
 )
 
 func getEvents(context *gin.Context) {
@@ -43,8 +44,22 @@ func getEvent(context *gin.Context) {
 
 func createEvent(context *gin.Context) {
 
+	token := context.Request.Header.Get("Authorization")
+
+	if token == "" {
+
+		context.JSON(http.StatusBadRequest, gin.H{"message": "No token given"})
+	}
+
+	err := utils.VerifyToken(token)
+
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
+		return
+	}
+
 	var event models.Event
-	err := context.ShouldBindJSON(&event)
+	err = context.ShouldBindJSON(&event)
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{
 			"message": "Could not parse request data.",
@@ -53,7 +68,6 @@ func createEvent(context *gin.Context) {
 		return
 	}
 
-	event.ID = 1
 	event.UserID = 1
 	event.DateTime = time.Now()
 	event.Save()
--- REST_API/utils/jwt.go
+++ REST_API/utils/jwt.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"errors"
+	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -13,4 +15,30 @@ func GenerateToken(email_id string, user_id int64) (output string, err error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{"email": email_id, "userID": user_id, "exp": time.Now().Add(time.Hour * 2).Unix()})
 	output, err = token.SignedString([]byte(secretkey))
 	return
+}
+
+func VerifyToken(token string) error {
+
+	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, errors.New("unexpectrd token error")
+
+		}
+		return []byte(secretkey), nil
+	})
+
+	if err != nil {
+		fmt.Println("Could not parse token")
+		return errors.New("could not parse token")
+	}
+
+	validToken := parsedToken.Valid
+
+	if !validToken {
+		return errors.New("invailid token")
+	}
+
+	return nil
 }
