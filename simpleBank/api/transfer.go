9e70e611f16826a8954b5d03ded3db258f748d1a
rpraveenkumar1203 <rpraveenkumar1203@outlook.com>
2024-10-07 23:16:24 +0530

    CREATED NEW API FOR transfer REQUEST
    (base) pkr@rpraveenkumar:~/Golang/Golang/simpleBank$ make test
    go test -v -cover ./...
            github.com/rpraveenkumar/Golang         coverage: 0.0% of statements
            github.com/rpraveenkumar/Golang/db/mock         coverage: 0.0% of statements
    === RUN   TestGetAccountAPi
    === RUN   TestGetAccountAPi/OK
    [GIN] 2024/10/07 - 23:16:15 | 200 |     127.738µs |                 | GET      "/accounts/527"
    === RUN   TestGetAccountAPi/NotFound
    [GIN] 2024/10/07 - 23:16:15 | 404 |      38.402µs |                 | GET      "/accounts/527"
    === RUN   TestGetAccountAPi/InternalError
    [GIN] 2024/10/07 - 23:16:15 | 500 |      15.205µs |                 | GET      "/accounts/527"
    === RUN   TestGetAccountAPi/InvalidInfo
    [GIN] 2024/10/07 - 23:16:15 | 400 |      14.403µs |                 | GET      "/accounts/0"
    --- PASS: TestGetAccountAPi (0.00s)
        --- PASS: TestGetAccountAPi/OK (0.00s)
        --- PASS: TestGetAccountAPi/NotFound (0.00s)
        --- PASS: TestGetAccountAPi/InternalError (0.00s)
        --- PASS: TestGetAccountAPi/InvalidInfo (0.00s)
    PASS
    coverage: 19.3% of statements
    ok      github.com/rpraveenkumar/Golang/api     0.006s  coverage: 19.3% of statements
    === RUN   TestCreateAccount
    --- PASS: TestCreateAccount (0.00s)
    === RUN   TestGetAccount
    --- PASS: TestGetAccount (0.00s)
    === RUN   TestUpdateAccount
    --- PASS: TestUpdateAccount (0.00s)
    === RUN   TestDeleteAccount
    --- PASS: TestDeleteAccount (0.00s)
    === RUN   TestListAccount
    --- PASS: TestListAccount (0.04s)
    === RUN   TestCreateEntry
    --- PASS: TestCreateEntry (0.00s)
    === RUN   TestGetEntry
    --- PASS: TestGetEntry (0.01s)
    === RUN   TestListEntries
    --- PASS: TestListEntries (0.03s)
    === RUN   TestTransferTx
    >> before: 902 944
    >> tx: 892 954
    >> tx: 882 964
    >> tx: 872 974
    >> tx: 862 984
    >> after: 852 994
    --- PASS: TestTransferTx (0.03s)
    === RUN   TestTransferTxdeadlock
    >> before: 167 811
    >> after: 167 811
    --- PASS: TestTransferTxdeadlock (0.05s)
    === RUN   TestCreateTransfer
    --- PASS: TestCreateTransfer (0.00s)
    === RUN   TestGetTransfer
    --- PASS: TestGetTransfer (0.01s)
    === RUN   TestListTransfer
    --- PASS: TestListTransfer (0.11s)
    PASS
    coverage: 77.2% of statements
    ok      github.com/rpraveenkumar/Golang/db/sqlc (cached)        coverage: 77.2% of statements
    === RUN   TestRandomInt
    --- PASS: TestRandomInt (0.00s)
    === RUN   TestRandomString
    --- PASS: TestRandomString (0.00s)
    === RUN   TestRandomOwner
    --- PASS: TestRandomOwner (0.00s)
    === RUN   TestRandomMoney
    --- PASS: TestRandomMoney (0.00s)
    === RUN   TestRandomCurrency
    --- PASS: TestRandomCurrency (0.00s)
    PASS
    coverage: 59.1% of statements
    ok      github.com/rpraveenkumar/Golang/db/utils        (cached)        coverage: 59.1% of statements
    (base) pkr@rpraveenkumar:~/Golang/Golang/simpleBank$
    
    POSTMAN
    
    {
        "transfer": {
            "id": 2021,
            "from_account_id": 2,
            "to_account_id": 2,
            "amount": 10000,
            "created_at": "2024-10-07T17:45:11.091231Z"
        },
        "from_account": {
            "id": 3,
            "owner": "pkr",
            "balance": 1100,
            "currency": "USD",
            "created_at": "2024-10-02T17:23:44.875563Z"
        },
        "to_account": {
            "id": 2,
            "owner": "pkr",
            "balance": 900,
            "currency": "USD",
            "created_at": "2024-10-02T17:23:43.661987Z"
        },
        "from_entry": {
            "id": 1456,
            "account_id": 3,
            "amount": -10000,
            "created_at": "2024-10-07T17:45:11.091231Z"
        },
        "to_entry": {
            "id": 1457,
            "account_id": 2,
            "amount": 10000,
            "created_at": "2024-10-07T17:45:11.091231Z"
        }
    }
    
    Changes to be committed:
    	modified:   simpleBank/api/server.go
    	new file:   simpleBank/api/transfer.go

---
+++ simpleBank/api/transfer.go
@@ -0,0 +1,83 @@
+package api
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	db "github.com/rpraveenkumar/Golang/db/sqlc"
+)
+
+// type transferRequest struct {
+// 	FromAccountId int64  `json:"from_account_id" binding:"required,min=1"`
+// 	ToAccountId   int64  `json:"to_account_id" binding:"required,min=1"`
+// 	Amount        int64  `json:"Amount" binding:"required,gt=1"`
+// 	Currency      string `json:"currency" binding:"required,oneof= USD EUR INR"`
+// }
+
+type transferRequest struct {
+	FromAccountId int64  `json:"from_account_id" binding:"required"` // No space after min=
+	ToAccountId   int64  `json:"to_account_id" binding:"required"`
+	Amount        int64  `json:"amount" binding:"required,gt=0"`
+	Currency      string `json:"currency" binding:"required,oneof=USD EUR INR"` // No space after oneof=
+}
+
+func (server *Server) createTransfer(ctx *gin.Context) {
+
+	req := transferRequest{}
+
+	err := ctx.ShouldBindJSON(&req)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, ErrorResponse(err))
+		return
+	}
+
+	if !server.ValidAccount(ctx, req.FromAccountId, req.Currency) {
+		return
+	}
+
+	if !server.ValidAccount(ctx, req.ToAccountId, req.Currency) {
+		return
+	}
+
+	arg := db.TransferTxParams{
+		FromAccountID: req.FromAccountId,
+		ToAccountID:   req.ToAccountId,
+		Amount:        req.Amount,
+	}
+
+	result, err := server.store.TransferTx(ctx, arg)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, ErrorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, result)
+
+}
+
+func (server *Server) ValidAccount(ctx *gin.Context, accountID int64, currency string) bool {
+
+	account, err := server.store.GetAccount(ctx, accountID)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+
+			ctx.JSON(http.StatusNotFound, ErrorResponse(err))
+			return false
+		}
+		ctx.JSON(http.StatusInternalServerError, ErrorResponse(err))
+		return false
+	}
+
+	if account.Currency != currency {
+		err := fmt.Errorf("account %d Currency mismatch : %s vs %s ", accountID, account.Currency, currency)
+		ctx.JSON(http.StatusBadRequest, ErrorResponse(err))
+		return false
+	}
+
+	return true
+}
